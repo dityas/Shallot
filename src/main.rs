a8abee64d771750728fd3ce0d60beb10f3224985
Aditya Shinde <shinde.aditya386@gmail.com>
2022-03-04 18:39:04 -0500

    accept connections on a single thread

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-fn run_server() { }
+mod logging;
+mod proxy_listener;
+
+fn run_server() { 
+    proxy_listener::run_listener()
+}
 
 fn main() {
     run_server();
--- src/proxy_listener.rs
+++ src/proxy_listener.rs
@@ -1,7 +1,10 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::Error;
 
+use crate::logging;
 
-fn run_listener(ip: &String, port: &String) -> Option<TcpListener> {
+// Create a simple TcpListener for given ip and port
+fn get_listener(ip: &String, port: &String) -> TcpListener {
     
     // Will remove all debugging lines after testing
     println!("Starting proxy listener on {}:{}", ip, port);
@@ -9,21 +12,67 @@ fn run_listener(ip: &String, port: &String) -> Option<TcpListener> {
     let listener = TcpListener::bind(format!("{}:{}", ip, port));
     let listener_handler = match listener {
 
-        Ok(sock) => Some(sock),
+        Ok(sock) => sock,
         Err(err) => {
             eprintln!("Could not open listener on {}:{}", ip, port);
             eprintln!("Encountered error {}", err);
+            panic!("Could not start server!");
+        },
+    };
+
+    println!("Listener started");
+
+    listener_handler
+}
+
+
+fn handle_request(stream: TcpStream) -> Result<(), Error> {
+
+    let addr = match stream.peer_addr() {
+
+        Ok(addr) => {
+            logging::log(addr);
+            Some(addr)
+        },
+
+        Err(e) => {
+            eprintln!("Could not get remote address, {}", e);
             None
         },
     };
 
-    // Return a Maybe TcpListener
-    listener_handler
+    Ok(())
+}
+
+
+pub fn run_listener() {
+
+    let ip = String::from("127.0.0.1");
+    let port = String::from("7878");
+
+    let listener = get_listener(&ip, &port);
+
+    // Wait for connections and handle them
+
+    for stream in listener.incoming() {
+       
+        match stream {
+
+            Ok(stream) => { 
+                handle_request(stream);
+            },
+
+            Err(e) => {
+                eprintln!("Error while processing connection, {}", e);
+            },
+        };
+    }
 }
 
 #[cfg(test)]
 mod test_proxy_listener {
     
+    use super::get_listener;
     use super::run_listener;
 
     #[test]
@@ -32,7 +81,15 @@ mod test_proxy_listener {
         let ip: String = String::from("127.0.0.1");
         let port: String = String::from("8080");
 
-        let result = run_listener(&ip, &port);
+        let result = get_listener(&ip, &port);
     }
+
+    #[test]
+    fn test_run_listener() {
+        
+        println!("Testing listener");
+        run_listener();
+    }
+
 }
 
