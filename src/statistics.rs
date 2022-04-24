b6dccc85176cbd798926d18becc87316c572f538
Shubhangi Rai <sr94042@uga.edu>
2022-04-24 07:31:54 -0400

    summary statistics

---
+++ src/statistics.rs
@@ -0,0 +1,66 @@
+use std::fs::File;
+use std::fs;
+use std::{thread, time};
+use notify::{Watcher, RecursiveMode, watcher};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+use std::io::{ErrorKind, Write, Error};
+use regex::Regex;
+
+
+pub fn generate_statistics() {
+    File::create("./statistics.txt").expect("Unable to create statistics file.");
+    let wait_time = time::Duration::from_secs(5);
+    /*
+
+    WhiteListDeny,
+    BlackListDeny,
+    Connection,
+    DataTransfer,
+    ProxyServer,
+    SuspiciousActivity,
+    Uncategorized,
+     */
+    loop {
+        let mut log = fs::read_to_string("./log.txt").expect("Unable to read log.txt");
+        let mut whitelist_deny = 0;
+        let mut blacklist_deny = 0;
+        let mut connection = 0;
+        let mut data_transfer = 0;
+        let mut proxy_server = 0;
+        let mut suspicious_activity = 0;
+        let mut uncategorised = 0;
+
+        for log_line in log.split("\n") {
+            if log_line.contains("Blacklist Deny") {
+                blacklist_deny += 1;
+            } else if log_line.contains("Whitelist Deny") {
+                whitelist_deny += 1;
+            } else if log_line.contains("Connection") {
+                connection += 1;
+            } else if log_line.contains("Data Transfer") {
+                data_transfer += 1;
+            } else if log_line.contains("Proxy Server") {
+                proxy_server += 1;
+            } else if log_line.contains("Suspicious Activity") {
+                suspicious_activity += 1;
+            } else {
+                uncategorised += 1;
+            }
+        }
+        let statistics_text = format!(
+            "Total number of connections: {}\n\
+            Number of whitelist deny events: {}\n\
+            Number of blacklist deny events: {}\n\
+            Number of data transfer events: {}\n\
+            Number of proxy server events: {}\n\
+            Number of suspicious activities events: {}\n\
+            Number of uncategorized events: {}",
+            connection, whitelist_deny, blacklist_deny, data_transfer,
+            proxy_server, suspicious_activity, uncategorised);
+
+        fs::write("./statistics.txt", statistics_text).expect("Unable to write");
+
+        thread::sleep(wait_time);
+    }
+}
