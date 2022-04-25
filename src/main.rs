a1d1382bbf6d1e8aa25bebcbd93da25426f06e05
Shubhangi Rai <sr94042@uga.edu>
2022-04-25 17:15:42 -0400

    Generate Statistics function outside the tokio::spawn

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,16 @@ mod firewall;
 mod logging;
 mod proxy_listener;
 mod request_handler;
+mod statistics;
 
+use std::thread;
+use statistics::generate_statistics;
 fn main() {
+    // Run generate_statistics in a background thread which generates the stats from event_log.txt
+    // every 5s seconds into statistics.txt file.
+    thread::spawn(|| {
+        generate_statistics();
+    });
     // Block the runtime on the proxy listener
     proxy_listener::run_listener();
     println!("Terminating server!");
