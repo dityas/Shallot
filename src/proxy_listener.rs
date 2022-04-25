ed3144b1c9adbda3e9a7d4ac4ddc22645ac21bd8
Aditya Shinde <shinde.aditya386@gmail.com>
2022-04-25 00:36:02 -0400

    welp, something works! Added basic CONNECT tunelling

--- src/proxy_listener.rs
+++ src/proxy_listener.rs
@@ -1,43 +1,46 @@
-use tokio::net::{TcpListener, TcpStream};
-use tokio::task;
-
-use std::io::Read;
 use std::net::SocketAddr;
-use std::rc::Rc;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
 
-use httparse::{EMPTY_HEADER, Request};
-
-use crate::logging;
 use crate::firewall::Firewall;
-use crate::request_handler::process_request;
-
+use crate::logging;
+use crate::request_handler::process_connection;
 
 // Req Handling error type
 pub type Result<T> = std::result::Result<T, ProxyError>;
 
 #[derive(Debug)]
 pub enum ProxyError {
+    IO(String),
+    Parse(String),
+    Other(String),
+    StreamClosed,
+    CannotConnectToDest,
+    IOBlocked,
+}
 
-    IOError(String),
-    ParseError(String),
-    OtherError(String),
+/// Open connection to the target and return the tcp stream
+pub fn get_target_stream(addr: &str) -> Result<TcpStream> {
+    match TcpStream::connect(addr) {
+        Ok(a) => Ok(a),
+        Err(_) => Err(ProxyError::CannotConnectToDest),
+    }
 }
 
 // Create a simple TcpListener for given ip and port
-async fn get_listener(ip: &String, port: &String) -> TcpListener {
-
+fn get_listener(ip: &String, port: &String) -> TcpListener {
     // Will remove all debugging lines after testing
     println!("Starting proxy listener on {}:{}", ip, port);
 
-    let listener = TcpListener::bind(format!("{}:{}", ip, port)).await;
+    let listener = TcpListener::bind(format!("{}:{}", ip, port));
     let listener_handler = match listener {
-
         Ok(sock) => sock,
         Err(err) => {
             eprintln!("Could not open listener on {}:{}", ip, port);
             eprintln!("Encountered error {}", err);
             panic!("Could not start server!");
-        },
+        }
     };
 
     println!("Listener started");
@@ -46,48 +49,49 @@ async fn get_listener(ip: &String, port: &String) -> TcpListener {
 }
 
 fn whitelist_check(addr: &SocketAddr, fwall: &mut Firewall) -> bool {
-
     match fwall.in_whitelist(addr.ip().to_string().as_str()) {
-
         true => {
             logging::event_log(
-                format!("[Connection event] Got request from {:?}", addr.ip()).as_str());
+                format!("[Connection event] Got request from {:?}", addr.ip()).as_str(),
+            );
             true
-        },
+        }
 
         false => {
             logging::event_log(
-                format!("[Firewall event] {} not in whitelist!", addr.ip()).as_str());
+                format!("[Firewall event] {} not in whitelist!", addr.ip()).as_str(),
+            );
 
             false
-        },
+        }
     }
 }
 
-pub async fn run_listener() -> Result<()>  {
-
+pub fn run_listener() {
     let ip = String::from("127.0.0.1");
     let port = String::from("7878");
 
-    let listener = get_listener(&ip, &port).await;
+    let listener = get_listener(&ip, &port);
     let mut firewall = Firewall::new();
 
-    loop {
+    for mut stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                thread::spawn(move || {
+                    match process_connection(&mut stream) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            eprintln!("Got error: {:?}", e);
+                        }
+                    };
+                });
+            }
 
-        let (stream, addr) = listener.accept().await
-            .map_err(|e| ProxyError::IOError(format!("Error: {:?}", e)))?;
-
-        //match whitelist_check(&addr, &mut firewall) {
-        match true {
-
-            true => {
-                process_request(&stream).await;
-            },
-
-            false => {
+            Err(e) => {
                 logging::event_log(
-                    format!("[Request denied] Sending reset to {}", addr.ip()).as_str());
-            },
+                    format!("[Connection event] Could not accept connection {:?}", e).as_str(),
+                );
+            }
         }
     }
 }
@@ -100,16 +104,9 @@ mod test_proxy_listener {
 
     #[test]
     fn test_listener_init() {
-
         let ip: String = String::from("127.0.0.1");
         let port: String = String::from("8080");
 
         let result = get_listener(&ip, &port);
-    }
-
-    #[test]
-    fn test_hyper_server() {
-
-
     }
 }
--- src/request_handler.rs
+++ src/request_handler.rs
@@ -1,15 +1,21 @@
+use std::fmt::Debug;
+use std::io::ErrorKind::WouldBlock;
+use std::io::Read;
+use std::io::Write;
 
-use std::future::Future;
-use std::convert::Infallible;
-use std::fmt::Display;
+use std::net::Shutdown;
+use std::net::TcpStream;
 
-use tokio::net::TcpStream;
-use httparse::{EMPTY_HEADER, Request};
+use httparse::{Request, EMPTY_HEADER};
 
 use crate::logging;
-use crate::proxy_listener::Result;
+use crate::proxy_listener::get_target_stream;
 use crate::proxy_listener::ProxyError;
+use crate::proxy_listener::Result;
 
+/// HTTP responses from the proxy server
+/// HTTP response for 200 OK
+const HTTP_OK: &[u8] = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
 
 /// Parse request into a well defined request type
 /// For now, the proxy only supports GET and CONNECT requests
@@ -20,7 +26,6 @@ pub enum ReqType {
 }
 
 impl std::fmt::Display for ReqType {
-
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             ReqType::CONNECT(a) => write!(f, "ReqType::CONNECT({})", a),
@@ -31,68 +36,216 @@ impl std::fmt::Display for ReqType {
 
 /// Determine request type. Get the raw request and parse it into
 /// a ReqType.
-async fn determine_request(buf: &[u8]) -> Result<ReqType> {
-
+fn determine_request(buf: &[u8]) -> Result<ReqType> {
     let len = buf.len();
     let mut headers = [EMPTY_HEADER; 4096];
     let mut req = Request::new(&mut headers);
-    let res = req.parse(&buf);
+    let res = req
+        .parse(&buf)
+        .map_err(|_| ProxyError::Parse("While parsing request".to_owned()))?;
 
-    match req.method {
-        Some("CONNECT") => match req.path {
-            Some(a) => Ok(ReqType::CONNECT(a.to_owned())),
-            None => Err(ProxyError::ParseError("Could not parse path".to_owned()))
-        },
+    if res.is_partial() {
+        Err(ProxyError::Parse("Incomplete request".to_owned()))
+    } else {
+        match req.method {
+            Some("CONNECT") => match req.path {
+                Some(a) => Ok(ReqType::CONNECT(a.to_string())),
+                None => Err(ProxyError::Parse("Could not parse path".to_owned())),
+            },
 
-        Some("GET") => match req.path {
-            Some(p) => Ok(ReqType::GET(p.to_owned())),
-            None => Err(ProxyError::ParseError("Could not parse path".to_owned())),
-        },
+            Some("GET") => match req.path {
+                Some(p) => Ok(ReqType::GET(p.to_string())),
+                None => Err(ProxyError::Parse("Could not parse path".to_owned())),
+            },
 
-        _ => Err(ProxyError::ParseError("Could not parse request".to_owned())),
+            Some(p) => Err(ProxyError::Parse(format!("Could not parse {}", p))),
+            None => Err(ProxyError::Parse("Could not parse request".to_owned())),
+        }
     }
 }
 
-async fn process_tcpstream_with<'a, T: Display, F, Fut>(
-    stream: &TcpStream,
-    buf: &'a mut [u8],
-    f: F) -> Result<T> 
+fn get_req_type(stream: &mut TcpStream) -> Result<ReqType> {
+    let mut buf = [0u8; 4096];
+    let read_bytes = read_from_tcpstream(stream, &mut buf)?;
 
-where
-    F: FnOnce(&'a [u8]) -> Fut,
-    Fut: Future<Output = Result<T>> + 'a {
-
-        stream.readable().await;
-        match stream.try_read(buf) {
-
-            Ok(0) => Err(ProxyError::IOError("0 bytes read from stream".to_owned())),
-            Ok(n) => f(buf).await,
-            Err(e) => Err(ProxyError::IOError(format!("Error: {:?}", e))),
-        }
+    determine_request(&mut buf[0..read_bytes])
 }
 
-pub async fn process_request(stream: &TcpStream) {
+/// Wrappers to read from and write to asyn TcpStream
+/// Wrapper to write to a stream
+fn write_to_tcpstream(stream: &mut TcpStream, buf: &[u8]) -> Result<usize> {
+    match stream.write_all(buf) {
+        Ok(()) => match stream.flush() {
+            Ok(_) => Ok(buf.len()),
+            Err(e) => Err(ProxyError::IO(format!("While flushing {:?}", e))),
+        },
+        Err(ref e) if e.kind() == WouldBlock => Err(ProxyError::IOBlocked),
+        Err(e) => Err(ProxyError::IO(format!("While writing {:?}", e))),
+    }
+}
 
-    let req_type = process_tcpstream_with(
-        stream, &mut [0u8; 4096], determine_request).await;
+/// Wrapper to read from a stream
+fn read_from_tcpstream(stream: &mut TcpStream, buf: &mut [u8]) -> Result<usize> {
+    match stream.read(buf) {
+        Ok(0) => Err(ProxyError::StreamClosed),
+        Ok(n) => Ok(n),
+
+        Err(ref e) if e.kind() == WouldBlock => Err(ProxyError::IOBlocked),
+        Err(e) => Err(ProxyError::IO(format!("While reading {:?}", e))),
+    }
+}
+
+fn tunnel_bytes(s_stream: &mut TcpStream, t_stream: &mut TcpStream) -> usize {
+    let mut bytes_transferred = 0usize;
+
+    let mut source_to_target_buf = [0u8; 4096];
+    let mut target_to_source_buf = [0u8; 4096];
+
+    let mut source_bytes = 0usize;
+    let mut target_bytes = 0usize;
+
+    s_stream.set_nonblocking(true);
+    t_stream.set_nonblocking(true);
+
+    // Tunnel request
+    loop {
+        // Read from source into buf
+        if source_bytes == 0 {
+            match read_from_tcpstream(s_stream, &mut source_to_target_buf) {
+                Ok(0) => {
+                    t_stream.shutdown(Shutdown::Both);
+                    break;
+                }
+                Ok(n) => {
+                    //logging::event_log(
+                    //    format!(
+                    //        "{} bytes read from {}",
+                    //        n,
+                    //        s_stream.local_addr().unwrap().ip()
+                    //    )
+                    //    .as_str(),
+                    //);
+
+                    source_bytes = n;
+                }
+                Err(ProxyError::IOBlocked) => {}
+                Err(e) => {
+                    t_stream.shutdown(Shutdown::Both);
+                    break;
+                }
+            };
+        }
+
+        if source_bytes > 0 {
+            match write_to_tcpstream(t_stream, &mut source_to_target_buf[0..source_bytes]) {
+                Ok(0) => {
+                    s_stream.shutdown(Shutdown::Both);
+                    break;
+                }
+                Ok(n) => {
+                    //logging::event_log(
+                    //    format!(
+                    //        "{} bytes written to {}",
+                    //        n,
+                    //        t_stream.peer_addr().unwrap().ip()
+                    //    )
+                    //    .as_str(),
+                    //);
+                    bytes_transferred += source_bytes;
+                    source_bytes = 0;
+                }
+                Err(ProxyError::IOBlocked) => {}
+                Err(e) => {
+                    s_stream.shutdown(Shutdown::Both);
+                    break;
+                }
+            };
+        }
+
+        // Read from target
+        if target_bytes == 0 {
+            match read_from_tcpstream(t_stream, &mut target_to_source_buf) {
+                Ok(0) => {
+                    s_stream.shutdown(Shutdown::Both);
+                    break;
+                }
+                Ok(n) => {
+                    target_bytes = n;
+                    //logging::event_log(
+                    //    format!(
+                    //        "{} bytes read from {}",
+                    //        n,
+                    //        t_stream.peer_addr().unwrap().ip()
+                    //    )
+                    //    .as_str(),
+                    //);
+                }
+                Err(ProxyError::IOBlocked) => {}
+                Err(e) => {
+                    s_stream.shutdown(Shutdown::Both);
+                    break;
+                }
+            };
+        }
+
+        if target_bytes > 0 {
+            match write_to_tcpstream(s_stream, &mut target_to_source_buf[0..target_bytes]) {
+                Ok(0) => {
+                    t_stream.shutdown(Shutdown::Both);
+                    break;
+                }
+                Ok(n) => {
+                    bytes_transferred += target_bytes;
+                    target_bytes = 0;
+                    //logging::event_log(
+                    //    format!(
+                    //        "{} bytes writte to {}",
+                    //        n,
+                    //        s_stream.local_addr().unwrap().ip()
+                    //    )
+                    //    .as_str(),
+                    //);
+                }
+                Err(ProxyError::IOBlocked) => {}
+                Err(e) => {
+                    t_stream.shutdown(Shutdown::Both);
+                    break;
+                }
+            };
+        }
+    }
+
+    bytes_transferred
+}
+
+pub fn process_connection(stream: &mut TcpStream) -> Result<()> {
+    let req_type = get_req_type(stream);
 
     match req_type {
         Ok(ReqType::CONNECT(p)) => {
             logging::event_log(&format!("[CONNECT Request] for {}", p));
-        },
-        
+
+            let mut t_stream = get_target_stream(&p)?;
+            t_stream.set_nodelay(true);
+            let res = write_to_tcpstream(stream, HTTP_OK)?;
+
+            logging::event_log(&format!("[Connection established]"));
+
+            let n = tunnel_bytes(stream, &mut t_stream);
+            logging::event_log(&format!("[Tunnel] {} bytes exchanged", n));
+        }
+
         Ok(ReqType::GET(p)) => {
             logging::event_log(&format!("[GET Request] for {}", p));
-        },
-        
+        }
+
         Err(e) => {
             logging::event_log(&format!("[{:?}] while parsing request", e));
-        },
+        }
     }
+
+    Ok(())
 }
 
 #[cfg(test)]
-mod test_req_handler {
-
-
-}
+mod test_req_handler {}
